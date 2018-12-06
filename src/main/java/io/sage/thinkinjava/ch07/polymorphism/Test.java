b33536e0a9657c80d0f2be2f7e68f75b9ed380f5
orgin <https://gitee.com/sage225/refactoring.git>
2018-12-06 21:54:54 +0800

    init

---
+++ src/main/java/io/sage/thinkinjava/ch07/polymorphism/Test.java
@@ -0,0 +1,13 @@
+package io.sage.thinkinjava.ch07.polymorphism;
+
+import static net.mindview.util.Print.print;
+
+/**
+ * @author Sage
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        print("think in java");
+    }
+}
